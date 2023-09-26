197548226d3c19ccbad5411ff0b9df04af5f0b17
Vladislav Grigorev <9059520084@mail.ru>
2023-09-26 20:57:43 +0700

    project create Study [ECLIPSE]

---
+++ src/Cycles.java
@@ -0,0 +1,8 @@
+
+public class Cycles {
+	public void culcFor(int first, int less) {
+		for (int i = first; i < less; i++) {
+			System.out.println("[CULC FOR] Hello number = " + i);
+		}
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+	public static void main(String args[]) {
+		System.out.println("Program Started");
+		
+		Cycles exampleCulces = new Cycles();
+		//exampleCulces.culcFor(0, 10);
+		
+	}
+}
