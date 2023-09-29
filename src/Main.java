1b680cffa3ad65b8dfb66cce04723b2530775829
Vladislav Grigorev <9059520084@mail.ru>
2023-09-29 14:00:25 +0700

    AccessModifierStd

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,6 @@ import java.util.Arrays;
 public class Main {
 	public static void main(String args[]) {
 		System.out.println("Program Started");
-		
-		CyclesStd exCulc = new CyclesStd();
-		
-		ArraysStd exArray = new ArraysStd(new int[5]);
-		
+
 	}
 }
