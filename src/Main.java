62819ac1508aa1ac3b2dd7199b09ac372767082d
Vladislav Grigorev <7eximmortal@vk.com>
2023-10-14 12:54:47 +0700

    Check email

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import Generalization.*;
 public class Main {
 	public static void main(String args[]) {
 		
-		Account<Integer> p = new Account<Integer>(5000, 50000);
+		Account<Integer> p = new Account<Integer>(50000, 50000);
 		System.out.println(p.getSum());
 	}
 }
